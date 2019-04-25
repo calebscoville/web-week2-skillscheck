a013b4856d61ddf106095a50638b21dd62ab4902
Caleb Scoville <calebscoville@Calebs-MacBook-Pro.local>
2019-04-25 16:26:19 -0600

    Second

--- javascript-4/student.js
+++ javascript-4/student.js
@@ -4,7 +4,7 @@
 //////////////////Step 1////////////////////
 // Create a new array called 'faveColors' and set it your three favorite colors as strings.
 
-// CODE HERE
+const faveColors = ['blue', 'yellow', 'black']
 
 //////////////////Step 2////////////////////
 // Create an object called 'me' that has these keys: firstname, superHeroName, homeTown, superPowers, superPowerXP, profileImage. 
@@ -21,12 +21,23 @@
 
 // The profileImage key will return this url `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`.
 
-// CODE HERE
+const me = {
+    firstName: 'Caleb',
+    superHeroName: 'Galactic Bot',
+    homeTown: 'Vancouver',
+    superPowers: ['laser nipples', 'perpetual barfing', 'see-through skin'],
+    superPowerXP: 'Math.floor(Math.random() * 100) + 1',
+    profileImage: `https://randomuser.me/api/portraits/med/lego/${Math.floor(Math.random() * 10) + 1 }.jpg`
+}
+console.log(me)
 
 //////////////////Step 3////////////////////
-// Create three variables to hold some data off your me object. The first variable should be 'regularName' that is the value of your firstName on the me object. The next variable is 'superName' which is the value of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
+// Create three variables to hold some data off your me object. The first variable should be 'regularName' 
+// that is the value of your firstName on the me object. The next variable is 'superName' which is the value 
+// of superHeroName on the me object. Last is 'homeTown' which will be the value of homeTown on the me object
 
-// CODE HERE
+const regularName
+const 
 
 //////////////////Step 4////////////////////
 //Create a function called 'setColor' that takes in one param called arr. Just to make sure that we only ever have three colors, use splice to trim the array to 3 colors. 
